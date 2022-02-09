f0df4120c0367ac94d0ad6453c527380585d8061
Roman <cborzyy@gmail.com>
2022-02-09 17:44:32 +0300

    Initial commit

---
+++ app/src/main/java/com/narcissus/marketplace/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.narcissus.marketplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
