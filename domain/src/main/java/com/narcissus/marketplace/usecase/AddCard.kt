92ad9fd0eb3a72b767334e7e2b30a90b4801cd27
ggendrick <evgenyscheprov@gmail.com>
2022-02-21 21:05:51 +0300

    addcard usecase refactored

--- domain/src/main/java/com/narcissus/marketplace/usecase/AddCard.kt
+++ domain/src/main/java/com/narcissus/marketplace/usecase/AddCard.kt
@@ -5,7 +5,6 @@ import com.narcissus.marketplace.util.ActionResult
 
 class AddCard(
     private val userRemoteRepository: UserRemoteRepository,
-    private val checkIfCardNumberIsValid: CheckIfCardNumberIsValid
 ) {
     suspend operator fun invoke(
         cardNumber: Long,
@@ -17,5 +16,20 @@ class AddCard(
             ActionResult.SuccessResult(true)
         } else ActionResult.ErrorResult("")//подумать, мб делать это не тут
 
+    }
+
+    private fun checkIfCardNumberIsValid(cardNumber: Long): Boolean {
+        val cardNumberStr = cardNumber.toString()
+        var sum: Int = Character.getNumericValue(cardNumberStr[cardNumberStr.length - 1])
+        val parity: Int = cardNumberStr.length % 2
+        for (i in cardNumberStr.length - 2 downTo 0) {
+            var summand: Int = Character.getNumericValue(cardNumberStr[i])
+            if (i % 2 == parity) {
+                val product = summand * 2
+                summand = if (product > 9) product - 9 else product
+            }
+            sum += summand
+        }
+        return (sum % 10 == 0)
     }
 }
