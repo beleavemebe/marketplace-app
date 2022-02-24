f96e7a594508cfa9eac3775daaadbfa9c1248271
ggendrick <evgenyscheprov@gmail.com>
2022-02-24 14:12:54 +0300

    added splashscreen

--- app/src/main/java/com/narcissus/marketplace/ui/MainActivity.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.narcissus.marketplace.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,11 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        binding.bottomNavigationView.visibility = View.GONE
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
         initBottomNavigation(navController)
+
     }
 
     private fun initBottomNavigation(navController: NavController) {
         binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.fragment_home)
+                binding.bottomNavigationView.visibility = View.VISIBLE
+        }
     }
 }
