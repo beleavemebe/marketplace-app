02cf7114a407a5b11491e31759ec0417b32e0ba7
beleavemebe <cborzyy@gmail.com>
2022-02-25 17:16:32 +0300

    Define home screen use cases in ServiceLocator

--- app/src/main/java/com/narcissus/marketplace/ui/home/HomeFragment.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 import com.narcissus.marketplace.R
 import com.narcissus.marketplace.databinding.FragmentHomeBinding
-import com.narcissus.marketplace.model.ProductPreview
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private var _binding: FragmentHomeBinding? = null
@@ -31,32 +30,26 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun fillRecyclerWithDummyContent() {
-        val dummyProducts = listOf(
-            ProductPreview("1", "", 1449,"Apple MacBook Pro 13","","",752,"","",3,152),
-            ProductPreview("2", "", 1299,"Apple MacBook Air 13","","",1021,"","",5,196),
-            ProductPreview("3", "", 2199,"Apple MacBook Pro 16","","",128,"","",4,65),
-        )
-
         adapter.items = listOf(
             HomeScreenItem.Header(
                 requireContext().getString(R.string.top_rated)
             ),
-            HomeScreenItem.ProductList(dummyProducts),
+            HomeScreenItem.ProductList(emptyList()),
 
             HomeScreenItem.Header(
                 requireContext().getString(R.string.top_sales)
             ),
-            HomeScreenItem.ProductList(dummyProducts),
+            HomeScreenItem.ProductList(emptyList()),
 
             HomeScreenItem.Header(
                 requireContext().getString(R.string.you_visited)
             ),
-            HomeScreenItem.ProductList(dummyProducts),
+            HomeScreenItem.ProductList(emptyList()),
 
             HomeScreenItem.Header(
                 requireContext().getString(R.string.explore)
             ),
-            HomeScreenItem.ProductList(dummyProducts)
+            HomeScreenItem.ProductList(emptyList())
         )
     }
 
