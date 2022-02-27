1432beb0c01dfe921cc7ec5559a9e8b91176a3e8
beleavemebe <cborzyy@gmail.com>
2022-02-27 20:56:40 +0300

    Refactor cart item UI

--- app/src/main/java/com/narcissus/marketplace/ui/cart/CartItemViewHolder.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/cart/CartItemViewHolder.kt
@@ -5,13 +5,13 @@ import com.narcissus.marketplace.R
 import com.narcissus.marketplace.databinding.ListItemCartBinding
 import com.narcissus.marketplace.model.CartItem
 
-class CartViewHolder(
+class CartItemViewHolder(
     private val binding: ListItemCartBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(cartItem: CartItem) = with(binding) {
-        cartItemName.text = cartItem.data.name
-        (cartItem.data.price.toString()+"$").also { cartItemPrice.text = it }
+        tvName.text = cartItem.data.name
+        tvPrice.text = itemView.context.getString(R.string.price_placeholder, cartItem.data.price)
         cartItemProductAmount.text = cartItem.count.toString()
-        cartItemIcon.setImageResource(R.drawable.product_img_example)
+        ivIcon.setImageResource(R.drawable.product_img_example)
     }
 }
