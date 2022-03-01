b0e2347e150939b353b179277265e0f46ad0313d
beleavemebe <cborzyy@gmail.com>
2022-03-01 14:27:17 +0300

    Define UI components for Compose

---
+++ app/src/main/java/com/narcissus/marketplace/ui/user/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.narcissus.marketplace.ui.user.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.toFontFamily
+import com.narcissus.marketplace.R
+
+
+val Montserrat = Font(R.font.montserrat_regular_ttf).toFontFamily()
+val MontserratMedium = Font(R.font.montserrat_medium_ttf).toFontFamily()
+val MontserratSemiBold = Font(R.font.montserrat_semibold_ttf).toFontFamily()
