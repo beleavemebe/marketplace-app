0001059499c229372454495651d06c4171a3f490
beleavemebe <cborzyy@gmail.com>
2022-03-02 19:07:56 +0300

    Fixes xDDDD

--- app/src/main/java/com/narcissus/marketplace/di/ServiceLocator.kt
+++ app/src/main/java/com/narcissus/marketplace/di/ServiceLocator.kt
@@ -1,8 +1,9 @@
 package com.narcissus.marketplace.di
 
-import com.narcissus.marketplace.CartLocalRepositoryImpl
-import com.narcissus.marketplace.ProductsPreviewRepositoryImpl
-import com.narcissus.marketplace.UserLocalRepositoryImpl
+import com.narcissus.marketplace.data.CartLocalRepositoryImpl
+import com.narcissus.marketplace.data.ProductsDetailsRepositoryImpl
+import com.narcissus.marketplace.data.ProductsPreviewRepositoryImpl
+import com.narcissus.marketplace.data.UserLocalRepositoryImpl
 import com.narcissus.marketplace.usecase.GetRandomProducts
 import com.narcissus.marketplace.usecase.GetRecentlyVisitedProducts
 import com.narcissus.marketplace.usecase.GetTopRatedProducts
@@ -10,8 +11,12 @@ import com.narcissus.marketplace.usecase.GetTopSalesProducts
 import com.narcissus.marketplace.usecase.GetCart
 import com.narcissus.marketplace.usecase.GetCartCost
 import com.narcissus.marketplace.usecase.GetCartItemsAmount
+import com.narcissus.marketplace.usecase.GetProductDetails
 
 object ServiceLocator {
+    private val productsDetailsRepository = ProductsDetailsRepositoryImpl()
+    val getProductDetails = GetProductDetails(productsDetailsRepository)
+
     private val productsPreviewsRepository = ProductsPreviewRepositoryImpl()
     val getTopRatedProducts = GetTopRatedProducts(productsPreviewsRepository)
     val getTopSalesProducts = GetTopSalesProducts(productsPreviewsRepository)
