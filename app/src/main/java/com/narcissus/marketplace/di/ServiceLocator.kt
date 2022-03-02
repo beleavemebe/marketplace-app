9a959c135a03104bdec5f80aaf2293a9d10fff5d
mingboevkh <mingboyev.kh@gmail.com>
2022-03-02 18:53:02 +0300

    remove extra use cases

--- app/src/main/java/com/narcissus/marketplace/di/ServiceLocator.kt
+++ app/src/main/java/com/narcissus/marketplace/di/ServiceLocator.kt
@@ -3,7 +3,13 @@ package com.narcissus.marketplace.di
 import com.narcissus.marketplace.CartLocalRepositoryImpl
 import com.narcissus.marketplace.ProductsPreviewRepositoryImpl
 import com.narcissus.marketplace.UserLocalRepositoryImpl
-import com.narcissus.marketplace.usecase.*
+import com.narcissus.marketplace.usecase.GetRandomProducts
+import com.narcissus.marketplace.usecase.GetRecentlyVisitedProducts
+import com.narcissus.marketplace.usecase.GetTopRatedProducts
+import com.narcissus.marketplace.usecase.GetTopSalesProducts
+import com.narcissus.marketplace.usecase.GetCart
+import com.narcissus.marketplace.usecase.GetCartCost
+import com.narcissus.marketplace.usecase.GetCartItemsAmount
 
 object ServiceLocator {
     private val productsPreviewsRepository = ProductsPreviewRepositoryImpl()
