5028434005d2b5fe6bbd8305f580348eda3e3b15
beleavemebe <cborzyy@gmail.com>
2022-03-03 14:07:35 +0300

    Inflate home screen via `flow.combine()`

--- app/src/main/java/com/narcissus/marketplace/ui/home/HomeFragment.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/home/HomeFragment.kt
@@ -24,7 +24,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentHomeBinding.bind(view)
         initRecyclerView()
-        fillRecyclerWithDummyContent()
         subscribeToViewModel()
     }
 
@@ -33,83 +32,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         binding.rvContent.adapter = adapter
     }
 
-    // TODO: move initial dummy content to HomeViewModel
-    private fun fillRecyclerWithDummyContent() {
-        adapter.items = listOf(
-            HomeScreenItem.Header(
-                requireContext().getString(R.string.top_rated)
-            ),
-            HomeScreenItem.LoadingProductList(),
-
-            HomeScreenItem.Header(
-                requireContext().getString(R.string.top_sales)
-            ),
-            HomeScreenItem.LoadingProductList(),
-
-            HomeScreenItem.Header(
-                requireContext().getString(R.string.you_visited)
-            ),
-            HomeScreenItem.LoadingProductList(),
-
-            HomeScreenItem.Header(
-                requireContext().getString(R.string.explore)
-            ),
-            HomeScreenItem.LoadingProductList()
-        )
-    }
-
     private fun subscribeToViewModel() {
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            observeTopRatedProducts()
-            observeTopSalesProducts()
-            observeRecentlyVisitedProducts()
-            observeRandomProducts()
+            viewModel.contentFlow.collect(adapter::setItems)
         }
-    }
-
-    private suspend fun observeTopRatedProducts() {
-        viewModel.topRatedFlow.collect { items ->
-            adapter.items = adapter.items.modifiedAt(
-                TOP_RATED_PRODUCTS_INDEX,
-                HomeScreenItem.ProductList(items)
-            )
-        }
-    }
-
-    private suspend fun observeTopSalesProducts() {
-        viewModel.topSalesFlow.collect { items ->
-            adapter.items = adapter.items.modifiedAt(
-                TOP_SALES_PRODUCTS_INDEX,
-                HomeScreenItem.ProductList(items)
-            )
-        }
-    }
-
-    private suspend fun observeRecentlyVisitedProducts() {
-        viewModel.recentlyVisitedFlow.collect { items ->
-            adapter.items = adapter.items.modifiedAt(
-                RECENTLY_VISITED_PRODUCTS_INDEX,
-                HomeScreenItem.ProductList(items)
-            )
-        }
-    }
-
-    private suspend fun observeRandomProducts() {
-        viewModel.randomFlow.collect { result ->
-            adapter.items = adapter.items.modifiedAt(
-                EXPLORE_PRODUCTS_INDEX,
-                HomeScreenItem.ProductList(result.data)
-            )
-        }
-    }
-
-    private fun List<HomeScreenItem>.modifiedAt(
-        index: Int,
-        with: HomeScreenItem
-    ): List<HomeScreenItem> {
-        val result = this.toMutableList()
-        result[index] = with
-        return result
     }
 
     private fun navigateToProductDetails(id: String) {
@@ -126,10 +52,5 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     companion object {
         const val HOME_SCREEN_MARGINS = 8
-
-        const val TOP_RATED_PRODUCTS_INDEX = 1
-        const val TOP_SALES_PRODUCTS_INDEX = 3
-        const val RECENTLY_VISITED_PRODUCTS_INDEX = 5
-        const val EXPLORE_PRODUCTS_INDEX = 7
     }
 }
--- app/src/main/java/com/narcissus/marketplace/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/home/HomeViewModel.kt
@@ -1,16 +1,18 @@
 package com.narcissus.marketplace.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.narcissus.marketplace.R
 import com.narcissus.marketplace.di.ServiceLocator
+import com.narcissus.marketplace.model.ProductPreview
+import com.narcissus.marketplace.ui.home.recycler.HomeScreenItem
 import com.narcissus.marketplace.usecase.GetRandomProducts
 import com.narcissus.marketplace.usecase.GetRecentlyVisitedProducts
 import com.narcissus.marketplace.usecase.GetTopRatedProducts
 import com.narcissus.marketplace.usecase.GetTopSalesProducts
-import com.narcissus.marketplace.util.ActionResult
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlin.random.Random
 
 class HomeViewModel(
     private val getTopRatedProducts: GetTopRatedProducts = ServiceLocator.getTopRatedProducts,
@@ -18,26 +20,45 @@ class HomeViewModel(
     private val getRandomProducts: GetRandomProducts = ServiceLocator.getRandomProducts,
     private val getRecentlyVisitedProducts: GetRecentlyVisitedProducts = ServiceLocator.getRecentlyVisitedProducts,
 ) : ViewModel() {
-    val topRatedFlow = flow {
-        delay(Random.Default.nextLong(250, 500))
-        val result = getTopRatedProducts() as ActionResult.SuccessResult
-        emit(result.data)
+    private val topRatedFlow = productListFlow {
+        getTopRatedProducts().getOrThrow()
     }
 
-    val topSalesFlow = flow {
-        delay(Random.Default.nextLong(250, 500))
-        val result = getTopSalesProducts() as ActionResult.SuccessResult
-        emit(result.data)
+    private val topSalesFlow = productListFlow {
+        getTopSalesProducts().getOrThrow()
     }
 
-    val randomFlow = flow {
-        delay(Random.Default.nextLong(250, 500))
-        val result = getRandomProducts() as ActionResult.SuccessResult
-        emit(result)
+    private val randomFlow = productListFlow {
+        getRandomProducts().getOrThrow()
     }
 
-    val recentlyVisitedFlow = getRecentlyVisitedProducts().map { products ->
-        delay(Random.Default.nextLong(250, 500))
-        products
+    private val recentlyVisitedFlow = getRecentlyVisitedProducts().map { recentlyVisited ->
+        HomeScreenItem.ProductList(recentlyVisited)
+    }
+
+    val contentFlow: Flow<List<HomeScreenItem>> = combine(
+        topRatedFlow,
+        topSalesFlow,
+        randomFlow,
+        recentlyVisitedFlow,
+    ) { topRated, topSales, random, recentlyVisited ->
+        listOf(
+            HomeScreenItem.Header(R.string.top_rated),
+            topRated,
+            HomeScreenItem.Header(R.string.top_sales),
+            topSales,
+            HomeScreenItem.Header(R.string.you_visited),
+            recentlyVisited,
+            HomeScreenItem.Header(R.string.explore),
+            random,
+        )
+    }
+
+    private fun productListFlow(block: suspend () -> List<ProductPreview>): Flow<HomeScreenItem> {
+        return flow {
+            emit(HomeScreenItem.LoadingProductList())
+            val products = block()
+            emit(HomeScreenItem.ProductList(products))
+        }
     }
 }
