7f668c1f16cb95ae34999476841f4c4931ab558a
beleavemebe <cborzyy@gmail.com>
2022-03-03 14:19:53 +0300

    Delete linter artifacts xDDD

--- report/updated_sources/app/src/main/java/com/narcissus/marketplace/ui/user/theme/Shape.kt
+++
@@ -1,8 +0,0 @@
-package com.narcissus.marketplace.ui.user.theme
-
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
-
-val Shapes = Shapes(
-    medium = RoundedCornerShape(HalfPadding),
-)
