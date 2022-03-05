ef79c6d582e64d36a8aad59a768cb82e1169a60c
beleavemebe <cborzyy@gmail.com>
2022-03-05 14:21:12 +0300

    Extract top-level okHttpClient

--- apiclient/src/main/java/com/narcissus/marketplace/apiclient/di/ApiClientKoinModule.kt
+++ apiclient/src/main/java/com/narcissus/marketplace/apiclient/di/ApiClientKoinModule.kt
@@ -9,14 +9,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
+internal val okHttpClient by lazy {
+    OkHttpClient().newBuilder()
+        .addInterceptor(ApiKeyInterceptor())
+        .build()
+}
+
 internal val retrofit: Retrofit by lazy {
 // base url is https://dummyproducts-api.herokuapp.com/api/v1/
 // must be added in local.properties project level
     Retrofit.Builder().baseUrl(BuildConfig.DUMMYPRODUCTSBASEURL)
-        .addConverterFactory(
-            GsonConverterFactory.create()
-        )
-        .client(OkHttpClient().newBuilder().addInterceptor(ApiKeyInterceptor()).build()).build()
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
 }
 
 internal val apiService by lazy {
