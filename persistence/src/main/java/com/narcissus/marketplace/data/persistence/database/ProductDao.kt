6740681333dd93468753546eee2c84edc95de033
beleavemebe <cborzyy@gmail.com>
2022-03-08 14:47:17 +0300

    Replace entities on conflict

--- persistence/src/main/java/com/narcissus/marketplace/data/persistence/database/ProductDao.kt
+++ persistence/src/main/java/com/narcissus/marketplace/data/persistence/database/ProductDao.kt
@@ -2,15 +2,17 @@ package com.narcissus.marketplace.data.persistence.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.narcissus.marketplace.data.persistence.model.ProductEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProductDao {
+    // TODO: add limit of ~15
     @Query("SELECT * FROM product ORDER BY created DESC")
     fun getProducts(): Flow<List<ProductEntity>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProduct(productEntity: ProductEntity)
 }
