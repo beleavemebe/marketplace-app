6acd625264be87dc2335162b856a2d75a3e5d21c
evgenkostrov <evgenkostrov@gmail.com>
2022-03-09 03:39:47 +0300

    modify CoilGradientTransformation.kt

--- app/src/main/java/com/narcissus/marketplace/ui/catalog/CoilGradientTransformation.kt
+++ app/src/main/java/com/narcissus/marketplace/ui/catalog/CoilGradientTransformation.kt
@@ -1,28 +1,27 @@
 package com.narcissus.marketplace.ui.catalog
 
+
 import android.graphics.*
 import androidx.core.graphics.applyCanvas
-import coil.bitmap.BitmapPool
+import androidx.core.graphics.createBitmap
 import coil.size.Size
 import coil.transform.Transformation
 
 class CoilGradientTransformation() : Transformation {
 
-    override fun key(): String = CoilGradientTransformation::class.java.name
+    override val cacheKey: String = javaClass.name
 
-    override suspend fun transform(pool: BitmapPool, input: Bitmap, size: Size): Bitmap {
+    override suspend fun transform(input: Bitmap, size: Size): Bitmap {
 
         val width = input.width
         val height = input.height
-        val centerX = width*0.75f
+        val centerX = width * 0.75f
         val centerY = 0f
-        val radius = width*0.7f
+        val radius = width * 0.7f
         val gradient = RadialGradient(
             centerX, centerY, radius, Color.TRANSPARENT, Color.BLACK,
             Shader.TileMode.CLAMP
         )
-        val config = input.config
-        val output = pool.get(width, height, config)
         val paintBitmap = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
         val paintRect = Paint().apply {
             isAntiAlias = true
@@ -30,11 +29,11 @@ class CoilGradientTransformation() : Transformation {
             color = Color.BLACK
             xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP)
         }
+        val output = createBitmap(width, height, input.config)
         output.applyCanvas {
             drawBitmap(input, 0f, 0f, paintBitmap)
             drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintRect)
         }
-        pool.put(input)
         return output
     }
 }
