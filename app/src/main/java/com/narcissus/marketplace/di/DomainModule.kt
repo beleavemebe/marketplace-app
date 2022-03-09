94e5f32515e2065762ca0fc202feab4a0dc78835
mingboevkh <mingboyev.kh@gmail.com>
2022-03-09 11:31:07 +0300

    remove redundant imports

--- app/src/main/java/com/narcissus/marketplace/di/DomainModule.kt
+++ app/src/main/java/com/narcissus/marketplace/di/DomainModule.kt
@@ -1,7 +1,20 @@
 package com.narcissus.marketplace.di
 
 import com.narcissus.marketplace.data.di.dataModule
-import com.narcissus.marketplace.usecase.*
+import com.narcissus.marketplace.usecase.GetRandomProducts
+import com.narcissus.marketplace.usecase.GetRecentlyVisitedProducts
+import com.narcissus.marketplace.usecase.GetTopRatedProducts
+import com.narcissus.marketplace.usecase.GetTopSalesProducts
+import com.narcissus.marketplace.usecase.GetProductDetails
+import com.narcissus.marketplace.usecase.GetCart
+import com.narcissus.marketplace.usecase.GetCartItemsAmount
+import com.narcissus.marketplace.usecase.GetCartCost
+import com.narcissus.marketplace.usecase.SetCartItemAmount
+import com.narcissus.marketplace.usecase.SetCartItemSelected
+import com.narcissus.marketplace.usecase.SelectAllCartItems
+import com.narcissus.marketplace.usecase.RemoveFromCart
+import com.narcissus.marketplace.usecase.RemoveSelectedCartItems
+
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
