be9e225ac015533404cf12c6b219fc599a7d4d44
ggendrick <evgenyscheprov@gmail.com>
2022-03-21 15:43:54 +0300

    contains string replaced by contains regex

--- domain/src/main/java/com/narcissus/marketplace/domain/usecase/SignInWithEmail.kt
+++ domain/src/main/java/com/narcissus/marketplace/domain/usecase/SignInWithEmail.kt
@@ -5,7 +5,7 @@ import com.narcissus.marketplace.domain.util.AuthResult
 
 class SignInWithEmail(private val userRepository: UserRepository) {
     suspend operator fun invoke(email: String, pass: String): AuthResult {
-        return if (email.contains("@") && email.contains(".")) {
+        return if (email.contains(Regex("@.*\\."))) {
             userRepository.signInWithEmail(email, pass)
         } else {
             AuthResult.WrongEmail
