b46e33b540189e8b028c19b461948315354aad6e
beleavemebe <cborzyy@gmail.com>
2022-03-24 20:01:11 +0300

    Copypaste current palette for User screen

--- ui/user/src/main/java/com/narcissus/marketplace/ui/user/theme/Theme.kt
+++ ui/user/src/main/java/com/narcissus/marketplace/ui/user/theme/Theme.kt
@@ -5,25 +5,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = GradientBackgroundEnd,
+    primaryVariant = Color.Black,
+    secondary = Purple200,
+    error = Red,
+    background = DarkBackground,
+    surface = DarkBackground,
+    onPrimary = Color(0xFFECE6E6),
+    onSecondary = Color(0xFFB6B5B5),
+    onBackground = Color.White,
+    onSurface = Color.White,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = GradientBackgroundEnd,
     primaryVariant = Purple700,
-    secondary = Teal200,
-
-//    Other default colors to override
-//    background = Color.White,
-//    surface = Color.White,
-//    onPrimary = Color.White,
-//    onSecondary = Color.Black,
-//    onBackground = Color.Black,
-//    onSurface = Color.Black,
+    secondary = Color(0xFF8E44EB),
+    error = Red,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.Black, // todo: fix palette
+    onSecondary = LightGrey, // todo: fix palette
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 )
 
 @Composable
