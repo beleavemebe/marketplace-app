d969e0676daa45773c75c6fed85d1db00b1a3e01
beleavemebe <cborzyy@gmail.com>
2022-03-26 19:15:04 +0300

    Delete dummy user

--- domain/src/main/java/com/narcissus/marketplace/domain/model/UserProfile.kt
+++ domain/src/main/java/com/narcissus/marketplace/domain/model/UserProfile.kt
@@ -6,10 +6,3 @@ data class UserProfile(
     val email: String,
     val iconUrl: String?
 )
-
-val dummyUser = UserProfile(
-    "some id",
-    "Joe Ordinary",
-    "example@gmail.com",
-    "https://301-1.ru/uploads/image/ha-ha-ya-zdes-zhivu_pOLMNliEp9.jpeg",
-)
