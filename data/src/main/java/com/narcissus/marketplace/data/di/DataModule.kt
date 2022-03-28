29d38a3fd477d73bbb45f9248ea11a682b0143d2
beleavemebe <beleavemebe@users.noreply.github.com>
2022-03-28 19:06:04 +0000

    [MegaLinter] Apply linters fixes

--- data/src/main/java/com/narcissus/marketplace/data/di/DataModule.kt
+++ data/src/main/java/com/narcissus/marketplace/data/di/DataModule.kt
@@ -2,8 +2,8 @@ package com.narcissus.marketplace.data.di
 
 import com.narcissus.marketplace.apiclient.di.apiClientModule
 import com.narcissus.marketplace.data.CartRepositoryImpl
-import com.narcissus.marketplace.data.OrderRepositoryImpl
 import com.narcissus.marketplace.data.DepartmentRepositoryImpl
+import com.narcissus.marketplace.data.OrderRepositoryImpl
 import com.narcissus.marketplace.data.ProductsDetailsRepositoryImpl
 import com.narcissus.marketplace.data.ProductsPreviewRepositoryImpl
 import com.narcissus.marketplace.data.UserRepositoryImpl
@@ -11,8 +11,8 @@ import com.narcissus.marketplace.data.firebase.di.Qualifiers.CartReference
 import com.narcissus.marketplace.data.firebase.di.firebaseModule
 import com.narcissus.marketplace.data.persistence.di.persistenceModule
 import com.narcissus.marketplace.domain.repository.CartRepository
-import com.narcissus.marketplace.domain.repository.OrderRepository
 import com.narcissus.marketplace.domain.repository.DepartmentRepository
+import com.narcissus.marketplace.domain.repository.OrderRepository
 import com.narcissus.marketplace.domain.repository.ProductsDetailsRepository
 import com.narcissus.marketplace.domain.repository.ProductsPreviewRepository
 import com.narcissus.marketplace.domain.repository.UserRepository
