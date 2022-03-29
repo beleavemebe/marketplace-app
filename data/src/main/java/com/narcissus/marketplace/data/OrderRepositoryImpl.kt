dafcf585dbecd345d5bbdec8954d3f0d0e82f9c9
ggendrick <evgenyscheprov@gmail.com>
2022-03-29 18:58:38 +0300

    oreders repository return type changed

--- data/src/main/java/com/narcissus/marketplace/data/OrderRepositoryImpl.kt
+++ data/src/main/java/com/narcissus/marketplace/data/OrderRepositoryImpl.kt
@@ -3,13 +3,14 @@ package com.narcissus.marketplace.data
 import com.narcissus.marketplace.domain.model.Order
 import com.narcissus.marketplace.domain.repository.OrderRepository
 import com.narcissus.marketplace.domain.util.ActionResult
+import kotlinx.coroutines.flow.Flow
 
 internal class OrderRepositoryImpl : OrderRepository {
-    override suspend fun getOrders(): ActionResult<List<Order>> {
+    override suspend fun getOrders(): Flow<List<Order>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun makeAnOrder(order: Order): ActionResult<Boolean> {
+    override suspend fun makeAnOrder(order: Order): Boolean {
         TODO("Not yet implemented")
     }
 }
