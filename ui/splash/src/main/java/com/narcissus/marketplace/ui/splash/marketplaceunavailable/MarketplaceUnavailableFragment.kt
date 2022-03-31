7d346999b348356e6ff856401141713ffc2a6afb
beleavemebe <cborzyy@gmail.com>
2022-03-31 16:00:53 +0300

    Fix layout issues

--- ui/splash/src/main/java/com/narcissus/marketplace/ui/splash/marketplaceunavailable/MarketplaceUnavailableFragment.kt
+++ ui/splash/src/main/java/com/narcissus/marketplace/ui/splash/marketplaceunavailable/MarketplaceUnavailableFragment.kt
@@ -3,6 +3,4 @@ package com.narcissus.marketplace.ui.splash.marketplaceunavailable
 import androidx.fragment.app.Fragment
 import com.narcissus.marketplace.ui.splash.R
 
-class MarketplaceUnavailableFragment : Fragment(R.layout.fragment_marketplace_unavailable) {
-
-}
+class MarketplaceUnavailableFragment : Fragment(R.layout.fragment_marketplace_unavailable)
