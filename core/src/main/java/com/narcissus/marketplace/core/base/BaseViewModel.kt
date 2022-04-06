c5aaad1af53a0a06b657024c44d07333aadd6fb4
beleavemebe <cborzyy@gmail.com>
2022-04-06 20:09:23 +0300

    Add BaseViewModel.kt

---
+++ core/src/main/java/com/narcissus/marketplace/core/base/BaseViewModel.kt
@@ -0,0 +1,30 @@
+package com.narcissus.marketplace.core.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<Err> : ViewModel() {
+
+    abstract fun mapThrowable(t: Throwable): Err
+
+    private val _errors = MutableSharedFlow<Err>(replay = 1)
+    val errors: Flow<Err> = _errors.asSharedFlow()
+
+    fun execute(block: suspend () -> Unit) {
+        viewModelScope.launch(exceptionHandler) {
+            block()
+        }
+    }
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        viewModelScope.launch {
+            val err = mapThrowable(throwable)
+            _errors.emit(err)
+        }
+    }
+}
